045ed21e3664d1e7fe61e284d87157da57c5f1bd
MarceloHenrique1711 <focasnunes10@gmail.com>
2025-08-06 11:10:03 -0300

    Atividade 1

---
+++ ex01-REVISAO.py
@@ -0,0 +1,21 @@
+import string
+
+def contator(texto):
+    texto = texto.lower()
+
+    texto = texto.translate(str.maketrans('', '', string.punctuation))
+
+    palavras = texto.split()
+
+    contagem = {}
+    for palavra in palavras:
+        contagem[palavra] = contagem.get(palavra, 0) + 1
+
+    return contagem
+
+
+frase = "Esse exercício é um exercício fácil ou difícil exercício"
+
+resultado = contator(frase)
+
+print(resultado)
